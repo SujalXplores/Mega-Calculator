3eb87e8f6449d0b450d51b46d1412f1f462f1f0e
SujalShah3234 <shahc9437@gmail.com>
2021-03-03 16:13:53 +0530

    bootstrap added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CuboidComponent } from './components/cuboid/cuboid.component';
 import { Pagenotfound404Component } from './components/pagenotfound404/pagenotfound404.component';
 
 const routes: Routes = [
+  { path: '', component: CuboidComponent },
   { path: 'cuboid', component: CuboidComponent },
   { path: 'pagenotfound', component: Pagenotfound404Component },
   { path: '**', redirectTo: '/pagenotfound' }
